d2a9057661acf541908b29f4cce697fe39302ec7
Schalk Venter <venterschalk@gmail.com>
2018-07-19 11:03:47 +0200

    Fixes issue with service worker

--- service-worker.js
+++ service-worker.js
@@ -1,249 +1,99 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
+const cacheId = 'cache-and-update-0-2-3';
+const cacheItems = [
+'css/bootstrap.css',
+'css/bootstrap.min.css',
+'css/font-awesome.min.css',
+'css/main.css',
+'css/main.scss',
+'css/roboto.css',
+'fonts/fontawesome-webfont.eot',
+'fonts/fontawesome-webfont.svg',
+'fonts/fontawesome-webfont.ttf',
+'fonts/fontawesome-webfont.woff',
+'fonts/fontawesome-webfont.woff2',
+'fonts/FontAwesome.otf',
+'fonts/glyphicons-halflings-regular.eot',
+'fonts/glyphicons-halflings-regular.svg',
+'fonts/glyphicons-halflings-regular.ttf',
+'fonts/glyphicons-halflings-regular.woff',
+'fonts/roboto-italic.ttf',
+'fonts/roboto-medium.ttf',
+'fonts/roboto-regular.ttf',
+'img/aip-logo.png',
+'img/c4sa-logo-white.png',
+'img/code4sa-logo.png',
+'img/logo-152.png',
+'img/logo.png',
+'img/nqabile-logo.png',
+'img/openup-logo.svg',
+'index.html',
+'js/helpers.js',
+'js/l10n.js',
+'js/main.js',
+'js/models.js',
+'js/topics.js',
+'js/vendor/backbone-1.3.3-min.js',
+'js/vendor/backbone.localStorage.js',
+'js/vendor/backbone.stickit-0.9.2.js',
+'js/vendor/bootstrap-3.3.1.min.js',
+'js/vendor/handlebars-v4.0.5.js',
+'js/vendor/jquery-1.11.2.min.js',
+'js/vendor/jquery.autogrow-textarea.js',
+'js/vendor/modernizr-2.8.3-respond-1.4.2.min.js',
+'js/vendor/moment-2.14.1.min.js',
+'js/vendor/polyglot.min.js',
+'js/vendor/progressbar-1.0.1.min.js',
+'js/vendor/underscore-1.8.3-min.js',
+'js/views/about.js',
+'js/views/add_story.js',
+'js/views/footer.js',
+'js/views/home.js',
+'js/views/settings.js',
+'js/views/story.js',
+'js/views/userinfo.js',
+]
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
 
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-workbox.precaching.precacheAndRoute([
-  {
-    "url": "css/bootstrap.css",
-    "revision": "d2ab08de4855f3f73d2ecec6da794293"
-  },
-  {
-    "url": "css/bootstrap.min.css",
-    "revision": "3ab3438f85ad9f9e27e1af1facf0a9c4"
-  },
-  {
-    "url": "css/font-awesome.min.css",
-    "revision": "4083f5d376eb849a458cc790b53ba080"
-  },
-  {
-    "url": "css/main.css",
-    "revision": "10367f41fd8eb054342c574374ab2205"
-  },
-  {
-    "url": "css/main.scss",
-    "revision": "21a6116d5f2a330820d7eb40d734af39"
-  },
-  {
-    "url": "css/roboto.css",
-    "revision": "6c29d4ac38400c59b24db1b1de74470d"
-  },
-  {
-    "url": "fonts/fontawesome-webfont.eot",
-    "revision": "25a32416abee198dd821b0b17a198a8f"
-  },
-  {
-    "url": "fonts/fontawesome-webfont.svg",
-    "revision": "d7c639084f684d66a1bc66855d193ed8"
-  },
-  {
-    "url": "fonts/fontawesome-webfont.ttf",
-    "revision": "1dc35d25e61d819a9c357074014867ab"
-  },
-  {
-    "url": "fonts/fontawesome-webfont.woff",
-    "revision": "c8ddf1e5e5bf3682bc7bebf30f394148"
-  },
-  {
-    "url": "fonts/fontawesome-webfont.woff2",
-    "revision": "e6cf7c6ec7c2d6f670ae9d762604cb0b"
-  },
-  {
-    "url": "fonts/FontAwesome.otf",
-    "revision": "5dc41d8fe329a22fa1ee9225571c843e"
-  },
-  {
-    "url": "fonts/glyphicons-halflings-regular.eot",
-    "revision": "7ad17c6085dee9a33787bac28fb23d46"
-  },
-  {
-    "url": "fonts/glyphicons-halflings-regular.svg",
-    "revision": "ff423a4251cf2986555523dfe315c42b"
-  },
-  {
-    "url": "fonts/glyphicons-halflings-regular.ttf",
-    "revision": "e49d52e74b7689a0727def99da31f3eb"
-  },
-  {
-    "url": "fonts/glyphicons-halflings-regular.woff",
-    "revision": "68ed1dac06bf0409c18ae7bc62889170"
-  },
-  {
-    "url": "fonts/roboto-italic.ttf",
-    "revision": "31fc50b4c526d006bf3333b5c04eefaa"
-  },
-  {
-    "url": "fonts/roboto-medium.ttf",
-    "revision": "88f29ea5a372d06f521395134f62ab91"
-  },
-  {
-    "url": "fonts/roboto-regular.ttf",
-    "revision": "38861cba61c66739c1452c3a71e39852"
-  },
-  {
-    "url": "img/aip-logo.png",
-    "revision": "86fc8df39dbacd67e45f43a26ef85b1c"
-  },
-  {
-    "url": "img/c4sa-logo-white.png",
-    "revision": "2b02d02f99791edc99f88c54622f8c9b"
-  },
-  {
-    "url": "img/code4sa-logo.png",
-    "revision": "831760fd7c320f62232260a40e81b93d"
-  },
-  {
-    "url": "img/logo-152.png",
-    "revision": "10e44a686c6935e9e13de24b04371dcc"
-  },
-  {
-    "url": "img/logo.png",
-    "revision": "5692704f8f735b57cb2ab9b32121c257"
-  },
-  {
-    "url": "img/nqabile-logo.png",
-    "revision": "a13beef0f7074b142c3105ba4cd2779f"
-  },
-  {
-    "url": "img/openup-logo.svg",
-    "revision": "b72b3f305d1a1ebfc5ff6cc47fb3efdd"
-  },
-  {
-    "url": "index.html",
-    "revision": "1dd3278c7d6341cfa3385f730dda01c4"
-  },
-  {
-    "url": "js/helpers.js",
-    "revision": "fa9e317549a3c54badf2928c45a4e89f"
-  },
-  {
-    "url": "js/l10n.js",
-    "revision": "9821b8b51e5fa72b65406bf5212facbd"
-  },
-  {
-    "url": "js/main.js",
-    "revision": "8070b2223a7d97b12de8ebca026959de"
-  },
-  {
-    "url": "js/models.js",
-    "revision": "5736f4ef972c5a9b1039c56de40b042f"
-  },
-  {
-    "url": "js/topics.js",
-    "revision": "3034d99b9a658304656e06313fb192d1"
-  },
-  {
-    "url": "js/vendor/backbone-1.3.3-min.js",
-    "revision": "1f542a03bb9e6203b95cc9d0c39e0af7"
-  },
-  {
-    "url": "js/vendor/backbone.localStorage.js",
-    "revision": "4e4921cfba0adb0c88a5e461222f28a6"
-  },
-  {
-    "url": "js/vendor/backbone.stickit-0.9.2.js",
-    "revision": "71fdbffdd7ae69972dbc42e3823c5ed8"
-  },
-  {
-    "url": "js/vendor/bootstrap-3.3.1.min.js",
-    "revision": "2616d3564578d8f845813483352802a9"
-  },
-  {
-    "url": "js/vendor/handlebars-v4.0.5.js",
-    "revision": "67c1431d8a06d7b2e31f86874b757eeb"
-  },
-  {
-    "url": "js/vendor/jquery-1.11.2.min.js",
-    "revision": "5790ead7ad3ba27397aedfa3d263b867"
-  },
-  {
-    "url": "js/vendor/jquery.autogrow-textarea.js",
-    "revision": "72673a8b22dd8505d7b6f9f7bfd4f921"
-  },
-  {
-    "url": "js/vendor/modernizr-2.8.3-respond-1.4.2.min.js",
-    "revision": "c5de01ee6da003c07e3155cb56c569c8"
-  },
-  {
-    "url": "js/vendor/moment-2.14.1.min.js",
-    "revision": "2b7d0faf3728e2b30b55ace597e2a8a5"
-  },
-  {
-    "url": "js/vendor/polyglot.min.js",
-    "revision": "c2dbbc8a9a7969161fb1d59e440fae13"
-  },
-  {
-    "url": "js/vendor/progressbar-1.0.1.min.js",
-    "revision": "1970f37f615a8339b6b249e396ee7d0d"
-  },
-  {
-    "url": "js/vendor/underscore-1.8.3-min.js",
-    "revision": "543feb1ecaf06ea516f8cec5f9f3f279"
-  },
-  {
-    "url": "js/views/about.js",
-    "revision": "e9f46f071b7b4b6d20e9463acff21d3b"
-  },
-  {
-    "url": "js/views/add_story.js",
-    "revision": "3816231be09302d15b021381e8d11051"
-  },
-  {
-    "url": "js/views/footer.js",
-    "revision": "ba5a52ae5d875b795e430fa2b90674a7"
-  },
-  {
-    "url": "js/views/home.js",
-    "revision": "b339422e4ae1282e30eba637d708d806"
-  },
-  {
-    "url": "js/views/settings.js",
-    "revision": "b98fc2bd541262c0b859fa2ab93d26d5"
-  },
-  {
-    "url": "js/views/story.js",
-    "revision": "3e62a2f3be84e45b14e81a473a8d2c97"
-  },
-  {
-    "url": "js/views/userinfo.js",
-    "revision": "a63ce0315c6ce598fae02ee41fbf575d"
-  },
-  {
-    "url": "offline.js",
-    "revision": "63d19eb9e30cd8efb07e0d8840bd87fb"
-  },
-  {
-    "url": "README.md",
-    "revision": "df91069dd6e9cd87e9a6165580ceabd5"
-  },
-  {
-    "url": "sw.js",
-    "revision": "e1181d1a273c73ef943419450a4a96e2"
-  },
-  {
-    "url": "workbox-config.js",
-    "revision": "ee285289a369ae3912c63ca9a67187c6"
+const ignoreRequest = ['.com']
+const ignoreRequestRegex = new RegExp(`(${ignoreRequest.join('(\/?)|\\')})`);
+
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(precache());
+});
+
+
+self.addEventListener('fetch', function(event) {
+  if (ignoreRequestRegex.test(event.request.url)) {
+    return false;
   }
-])
-workbox.precaching.suppressWarnings();
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
-workbox.routing(
-  new RegExp('/'),
-  workbox.strategies.networkFirst()
-);
-workbox.googleAnalytics.initialize();
+
+  event.respondWith(fromCache(event.request));
+  event.waitUntil(update(event.request));
+  return true;
+});
+
+
+function precache() {
+  return caches.open(cacheId).then(function (cache) {
+    return cache.addAll(cacheItems);
+  });
+}
+
+
+function fromCache(request) {
+  return caches.open(cacheId).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
+    });
+  });
+}
+
+
+function update(request) {
+  return caches.open(cacheId).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response);
+    });
+  });
+}
